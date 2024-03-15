89507d883b7a54d9bfd57e2f47e07766b2595a08
aijisjtu <805109684aiji@sjtu.edu.cn>
2024-03-15 17:29:46 +0800

    创建个人中心页面第1部分

--- web/src/views/user/bot/userBotIndexView.vue
+++ web/src/views/user/bot/userBotIndexView.vue
@@ -5,11 +5,97 @@
 </template>
 <script>
 import ContentField from '../../../components/ContentField.vue'
+import $ from 'jquery'
+import { useStore } from 'vuex'
 
 export default{
     components:{
         ContentField
+    },
+    setup(){
+        const store = useStore();
+
+        // $.ajax({
+        //     url:"http://127.0.0.1:3000/user/bot/add/",
+        //     type:"POST",
+        //     data:{
+        //         title: "Bot的标题",
+        //         description: "Bot的描述",
+        //         content: "Bot的代码",
+        //     },
+        //     headers:{
+        //         Authorization: "Bearer "+store.state.user.token,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+
+        // $.ajax({
+        //     url:"http://127.0.0.1:3000/user/bot/remove/",
+        //     type:"POST",
+        //     data:{
+        //         bot_id: 2,
+        //     },
+        //     headers:{
+        //         Authorization: "Bearer "+store.state.user.token,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+
+        
+        // $.ajax({
+        //     url:"http://127.0.0.1:3000/user/bot/update/",
+        //     type:"POST",
+        //     data:{
+        //         bot_id: 2,
+        //         title: "Bot的标题77",
+        //         description: "Bot的描述77",
+        //         content: "Bot的代码77",
+        //     },
+        //     headers:{
+        //         Authorization: "Bearer "+store.state.user.token,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+
+
+        $.ajax({
+            url:"http://127.0.0.1:3000/user/bot/getlist/",
+            type:"get",
+            data:{
+                title: "Bot的标题",
+                description: "Bot的描述",
+                content: "Bot的代码",
+            },
+            headers:{
+                Authorization: "Bearer "+store.state.user.token,
+            },
+            success(resp) {
+                console.log(resp);
+            },
+            error(resp) {
+                console.log(resp);
+            }
+        })        
+
+
     }
 
-}</script>
+}
+
+</script>
 <style scoped></style>
